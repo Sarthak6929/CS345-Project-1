cb523b9e03ca2e141b9a8925612c35e3b9fe6904
Sarthak Sharma <66264034+Sarthak6929@users.noreply.github.com>
2023-03-10 12:39:19 -0500

    Enemy
    New update

---
+++ Assets/Scripts/ENEMY.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENEMY : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+    //private float timebtwShots;
+    //public float startTimebtwShots;
+    //public GameObject projectile;
+
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //timebtwShots = startTimebtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+        } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+
+        } else if (Vector2.Distance(transform.position, player.position) < retreatDistance) {
+
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+        //if (timebtwShots <= 0)
+        //{
+        //    //Instantiate(projectile, transform.position, Quaternion.identity);
+        //    //timebtwShots = startTimebtwShots;
+        //}
+        //else
+        //{
+        //    timebtwShots -= Time.deltaTime;
+        //}
+        
+    }
+}
